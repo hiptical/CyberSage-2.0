67577eeb80c49167663571b0ec2fa0d07c24ee56
Salehin Ashfi <124058914+AsHfIEXE@users.noreply.github.com>
2025-10-20 01:59:33 +0600

    Updated files

--- backend/app.py
+++ backend/app.py
@@ -191,4 +191,5 @@ if __name__ == '__main__':
                  host='0.0.0.0', 
                  port=5000, 
                  debug=True,
-                 use_reloader=False)
+                 use_reloader=False,
+                 allow_unsafe_werkzeug=True)
